8f426ad4b465a870fba59fa8933cc4963e3063ac
doctafaustus <clowd150@gmail.com>
2025-09-12 15:25:39 -0500

    Add up to chapter 54

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { type TasksComponent } from './task.model';
+import { NewTaskComponent } from '../new-task/new-task.component';
+
+@Component({
+  selector: 'app-task',
+  imports: [NewTaskComponent],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+export class TaskComponent {
+  @Input({ required: true }) task!: TasksComponent;
+  @Output() complete = new EventEmitter<string>();
+
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-// type User = {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// };
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -20,6 +9,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
@@ -27,7 +17,6 @@ export class UserComponent {
   }
 
   onSelectUser() {
-    console.log('Clicked!', this);
     this.select.emit(this.user.id);
   }
 }

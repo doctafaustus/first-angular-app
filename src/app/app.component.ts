12004891d31f142acea9a8cf43d8d633d650d63e
doctafaustus <clowd150@gmail.com>
2025-09-16 15:38:20 -0500

    Complete Section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
-import { Component, Input, computed } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
+import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
